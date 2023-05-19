51a138b1f4bd84177fd1d9d17181318ca90f815a
azlanazim <129579557+azlanazim@users.noreply.github.com>
2023-05-19 21:23:39 +0800

    Updated myapps file

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from . import views
+from myapp.views import (
+    home_screen_view
+
+)
 
 urlpatterns = [
 
-    path('',views.index, name='index'),
-
+    path('',home_screen_view, name='home'),
+    
 
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -4,5 +4,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+    
 def index(request):
     return HttpResponse("Welcome to Stupendous Sloths")
+
+def home_screen_view(request):
+    print(request.headers)
+    return render(request, "base.html",{})
